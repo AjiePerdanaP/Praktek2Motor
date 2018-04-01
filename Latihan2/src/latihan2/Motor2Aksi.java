18d4523e58e1269439ee5840ba8beb85c8db979a
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-01 14:22:10 +0700

    Mengisi Nilai Atribut

--- Latihan2/src/latihan2/Motor2Aksi.java
+++ Latihan2/src/latihan2/Motor2Aksi.java
@@ -4,5 +4,8 @@ package latihan2;
 public class Motor2Aksi {
     public static void main(String[] args) {
         Motor2 m1 = new Motor2 ();
+        m1.merk="revo";
+        m1.harga=1.5000;
+        m1.jumlah_pesan= 4.0;
     }
 }
