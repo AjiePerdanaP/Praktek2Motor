aa90f28b7e2261b30bedb92dd295d5e17d0575ef
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-01 14:11:56 +0700

    Membuat atribut dan behavior

--- Latihan2/src/latihan2/Motor2.java
+++ Latihan2/src/latihan2/Motor2.java
@@ -1,5 +1,13 @@
 package latihan2;
 
 public class Motor2 {
+    String merk;
+    Double harga;
+    Double jumlah_pesan;
     
+    void mengisi_bensin(){
+        System.out.println("merk :"+merk);
+        System.out.println("harga :"+harga);
+        System.out.println("jumlah pesan :"+jumlah_pesan);
+    }
 }
